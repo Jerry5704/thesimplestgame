bc7d9d43ce6128e06576039734209ebd4dda0c8f
Jerry5704 <pawel.jozef.jurkiewicz@gmail.com>
2023-12-29 18:26:34 +0100

    added saving mechanism

--- src/scripts/script.py
+++ src/scripts/script.py
@@ -1,3 +1,5 @@
+import json
+
 class Meeting:
     def __init__(self, player1, player2):
         self.player1 = player1
@@ -7,13 +9,27 @@ class Meeting:
 
     def start_meeting(self):
         while (0, 0) not in self.result:
+            print(self.player1.points)
+            print(self.player2.points)
             # ToDo: change hardcoded player1 starter
             self.starter = self.set_starter(self.player1)
             self.ask_for_choice()
             self.add_result()
-            print(self.result[-1])
-        self.add_result_to_points()
+        self.write_to_json()
 
+    def write_to_json(self):
+        with open("/home/jerry/thesimplestgame/src/data/players.json", "r") as json_file:       
+            json_data = json.load(json_file)
+            for player in json_data["players"]:
+                if player["id"] == self.player1.id:
+                    player["points"] = self.player1.points            
+            for player in json_data["players"]:
+                if player["id"] == self.player2.id:
+                    player["points"] = self.player2.points            
+        
+        with open("/home/jerry/thesimplestgame/src/data/players.json", "w") as json_file:
+            json.dump(json_data, json_file, indent=2)
+    
     def set_starter(self, starter):
         self.starter = starter
 
@@ -28,14 +44,11 @@ class Meeting:
                 self.result.append((1, 1))
         else:
             if self.player1.choice == False:
-                self.result.append((2, 0))
+                self.result.append((2, -1))
             else:
-                self.result.append((0, 2))
-
-    def add_result_to_points(self):
-        for round in self.result:
-            self.player1.points += round[0]
-            self.player2.points += round[1]
+                self.result.append((-1, 2))
+        self.player1.points += self.result[-1][0]
+        self.player2.points += self.result[-1][1]
 
     def ask_for_choice(self):
         if input("Player1 choice [1/0]: ") == "1":
@@ -48,11 +61,21 @@ class Meeting:
             self.player2.set_choice(False)
 
 class Player:
-    def __init__(self) -> None:
-        self.name = str
-        self.points = int
+    def __init__(self, id):
+        self.id = id
+        self.player_data = self.get_player_data()
+        self.name = self.player_data["name"]
+        self.points = self.player_data["points"]
         self.choice = bool
     
+    def get_player_data(self):
+        with open("/home/jerry/thesimplestgame/src/data/players.json", "r") as players_data_json_file:
+            json_data = json.load(players_data_json_file)
+            for player in json_data["players"]:
+                if player["id"] == self.id:
+                    return player
+            return {"id": -1, "name": "Jan Pawel Tester", "score": 2137}
+
     def set_name(self, name):
         self.name = name
 
@@ -71,20 +94,21 @@ class Player:
     def get_choice(self):
         return self.choice
 
-
 def main():
-    player1 = Player()
-    player1.set_name("Marcin")
-    player1.set_points(0)
-    
-    player2 = Player()
-    player2.set_name("Janusz")
-    player2.set_points(0)
+    player0 = Player(0)
+    player1 = Player(1)
 
-    meeting = Meeting(player1, player2)
+    meeting = Meeting(player0, player1)
     meeting.start_meeting()
-    print(player1.get_points())
-    print(player2.get_points())
+    meeting.start_meeting()
+
+    print()
+    print(player0.name)
+    print(player0.points)
+    print()
+    print(player1.name)
+    print(player1.points)
+    print()
 
 if __name__ == "__main__":
     main()
